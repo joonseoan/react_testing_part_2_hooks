4a65e62990804439a3920dd2d63083b8dc2cb9d3
Joon An <joonseo.an@outlook.com>
2021-08-12 07:57:58 -0400

    setup hook testing

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      App COMPO
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Congrats/Congrats.tsx
@@ -0,0 +1,10 @@
+/**
+ * Congrats message page.
+ * @param {Object} props - React props.
+ * @returns {JSX.Element} - rendered component.
+ */
+const Congrats = ({ success }) => {
+  return <div />
+}
+
+export default Congrats;
