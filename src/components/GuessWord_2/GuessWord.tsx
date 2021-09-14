df2435728844dbedbf0ea168937e1fc543741cf6
JoonAN-Davinci <82390428+JoonAN-Davinci@users.noreply.github.com>
2021-09-14 07:55:13 -0400

    fixed issues

--- src/components/GuessWord_2/GuessWord.tsx
+++ src/components/GuessWord_2/GuessWord.tsx
@@ -2,54 +2,58 @@ import React, { FC } from 'react';
 import PropTypes from 'prop-types';
 
 interface GuessedWord {
-  guessedWord: String;
-  letterMatchCount: Number;
-};
+  guessedWord: string,
+  letterMatchCount: number,
+}
+
 interface GuessedWordsProps {
   guessedWords: GuessedWord [];
-};
+}
 
 // any: just for testing. (In reality, typescript should not use propTypes.)
 const GuessedWords: FC<GuessedWordsProps | any> = (props) => {
   let contents: JSX.Element;
 
-  console.log('props: ', props);
 
-  if (props.guessedWords.length === 0) {
-    contents = (
-      <span data-test="guess-instructions">
-        Try to guess the correct word!
-      </span>
-    )
-  } else {
-    const guessedWordsRows = props.guessedWords.map((word: GuessedWord , index: React.Key) => {
-      if (index) {
-        return (
-          <tr data-test="guessed-word" key={index}>
-            <td>{word.guessedWord}</td>
-            <td>{word.letterMatchCount}</td>
-          </tr>
-        );
-      } else {
-        return null;
-      }
-    });
-
-    contents = (
-      <div data-test="guessed-words">
-        <h3>Guessed Words</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>Guess</th><th>Matching Letters</th>
+  if (props && props.guessedWords) {
+    if (props.guessedWords.length === 0) {
+      contents = (
+        <span data-test="guess-instructions">
+          Try to guess the correct word!
+        </span>
+      )
+    } else {
+      const guessedWordsRows = props.guessedWords.map((word: GuessedWord , index: React.Key) => {
+        if (index) {
+          return (
+            <tr data-test="guessed-word" key={index}>
+              <td>{word.guessedWord}</td>
+              <td>{word.letterMatchCount}</td>
             </tr>
-          </thead>
-          <tbody>
-            {guessedWordsRows}
-          </tbody>
-        </table>
-      </div>
-    )
+          );
+        } else {
+          return null;
+        }
+      });
+  
+      contents = (
+        <div data-test="guessed-words">
+          <h3>Guessed Words</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th><th>Matching Letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {guessedWordsRows}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
+  } else {
+    contents = <div />;
   }
   
   return (
