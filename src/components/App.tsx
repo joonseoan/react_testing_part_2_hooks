76008ccb7d58cc445b5ea1640a5b00a82283c5e1
JoonAN-Davinci <82390428+JoonAN-Davinci@users.noreply.github.com>
2021-09-16 07:55:12 -0400

    added input component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,12 @@
 import GuessedWords from "./GuessWord_2/GuessWord";
+import Congrats from "./Congrats_1/Congrats";
+
 function App() {
   return (
-    <div className="App">
-      <GuessedWords />
+    <div className="container" style={{ textAlign: 'center' }}>
+      <h1>Jotto</h1>
+      <Congrats success={true} />
+      <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3 }]} />
     </div>
   );
 }
--- src/components/GuessWord_2/GuessWord.tsx
+++ src/components/GuessWord_2/GuessWord.tsx
@@ -33,8 +33,8 @@ const GuessedWords: FC<GuessedWordsProps | any> = ({ guessedWords = [] }) => {
       contents = (
         <div data-test="guessed-words">
           <h3>Guessed Words</h3>
-          <table>
-            <thead>
+          <table className="table table-sm">
+            <thead className="thead-light">
               <tr>
                 <th>Guess</th><th>Matching Letters</th>
               </tr>
