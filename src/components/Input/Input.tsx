183d494dd56016c6b5a4d652071bc7d78e2b83b3
joon <joonseo.an@outlook.com>
2021-09-20 07:55:17 -0400

    added useState testing

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,15 +1,31 @@
-import { FC } from "react";
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
 
-// ? just for now testing. 
+// ?: for now testing with propType.
 export interface InputProps {
     secretWord?: string 
 }
 
-const Input: FC<InputProps> = ({ secretWord }) => {
+// React.FC (no destruction) : for now to test
+const Input: React.FC<InputProps> = ({ secretWord }) => {
+
+    const [currentGuess, setCurrentGuess] = useState<string>('');
+
     return (
         <div data-test="component-input">
-
+            <form className="form-inline">
+                <input 
+                    data-test="input-box"
+                    className="mb-2 mx-sm-3"
+                    type="text"
+                    placeholder="enter guess"
+                    value={currentGuess}
+                    onChange={(event) => setCurrentGuess(event.target.value)}
+                />
+                <button data-test="submit-button" className="btn btn-primary mb-2">
+                    Submit
+                </button>
+            </form>
         </div>
     );
 }
