12dbd9c09503b1e0f61bc14fcba1d25d863ef89b
Joon Seo An <joonseo.an@outlook.com>
2021-10-13 08:03:52 -0400

    add skip for testing

--- src/components/AppState.test.tsx
+++ src/components/AppState.test.tsx
@@ -32,14 +32,14 @@ const setup = ({
   return wrapper;
 };
 
-describe("no words guessed", () => {
+describe.skip("no words guessed", () => {
   let wrapper: ReactWrapper;
 
   beforeEach(() => {
     wrapper = setup({
       secretWord: "party",
-      guessedWords: [],
       success: false,
+      guessedWords: [],
     });
   });
 
@@ -49,10 +49,58 @@ describe("no words guessed", () => {
   });
 });
 
-describe("some words guessed", () => {
-  test("a", () => {});
+describe.skip("some words guessed", () => {
+  let wrapper: ReactWrapper;
+
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      // "a" matched in party and agile.
+      guessedWords: [{ guessedWord: "agile", letterMatchCount: 1 }],
+    });
+  });
+
+  test("creates GuessedWords table with one row", () => {
+    const guessedWordRow = findByTestAttr(wrapper, "guessed-word");
+    // why 2?
+    expect(guessedWordRow).toHaveLength(2);
+  });
 });
 
-describe("guess secret word", () => {
-  test("a", () => {});
+describe.skip("guess secret word", () => {
+  let wrapper: ReactWrapper;
+
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      // "a" matched in party and agile.
+      guessedWords: [{ guessedWord: "agile", letterMatchCount: 1 }],
+    });
+
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    inputBox.simulate("change", { target: { value: "party" } });
+
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate("click", { preventDefault() {} });
+  });
+
+  test("add row to guessedWords table", () => {
+    const guessedWordNodes = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordNodes).toHaveLength(3);
+  });
+
+  test("displays congrats component", () => {
+    const congrats = findByTestAttr(wrapper, "component-congrats");
+    expect(congrats.text().length).toBeGreaterThan(0);
+  });
+
+  test("does not display input component contents", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    expect(inputBox.exists()).toBe(false);
+
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    expect(submitButton.exists()).toBe(false);
+  });
 });
