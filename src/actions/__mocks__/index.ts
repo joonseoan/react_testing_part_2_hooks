e724917d796be0cae15a1bed656667e68ecad918
Joon Seo An <joonseo.an@outlook.com>
2021-10-19 08:01:57 -0400

    setup jest.mock

--- src/actions/__mocks__/index.ts
+++ src/actions/__mocks__/index.ts
@@ -1,4 +1,12 @@
-module.exports = {
+// module.exports = {
+//   ...jest.requireActual(".."),
+//   __esModule: true,
+//   // TODO: update return value for Redux / Context API
+//   getSecretWord: jest.fn().mockReturnValue(Promise.resolve("party")),
+// };
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
   ...jest.requireActual(".."),
   __esModule: true,
   // TODO: update return value for Redux / Context API
--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,24 +1,40 @@
+// useEffect test.
+
 import { findByTestAttr } from "../test/testUtil";
-import { shallow, ShallowWrapper } from "enzyme";
+import { shallow, mount, ShallowWrapper, ReactWrapper } from "enzyme";
 import App from "./App";
+import { ReactNode } from "react";
 
 // activate global mock to make sure getSecretWord does not make network call
 // Yeah. it is index.ts in "./action".
 // The way of mocking the function.
 jest.mock("./actions");
 
+// This mock function getSecretWord action now is going to be "getSecretWord" in project "__mock__"
+// import { getSecretWord as mockGetSecretWord } from "../actions";
+
 /**
  * Setup ShallowWrapper.
- * @returns {ShallowWrapper}
+ * @returns {ReactNode}
  */
 const setup = () => {
-  return shallow(<App />);
+  // return shallow(<App />);
+  // [Important]: useEffect is called on Shallow.
+  // https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 };
 
-let wrapper: ShallowWrapper;
+// let wrapper: ShallowWrapper;
+let wrapper: ReactWrapper;
 
-test("renders learn react link", () => {
+test("renders without error", () => {
   wrapper = setup();
   const appComponent = findByTestAttr(wrapper, "component-app");
   expect(appComponent).toHaveLength(1);
+});
+
+describe("get secret word", () => {
+  test("get getSecretWord on app mount", () => {});
+
+  test("getSecretWord does not run on app update", () => {});
 });
