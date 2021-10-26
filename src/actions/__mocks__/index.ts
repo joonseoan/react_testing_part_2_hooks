afc4ca1ecf4f52753280ccb2925630fa6d2f8239
Joon Seo An <joonseo.an@outlook.com>
2021-10-26 08:03:02 -0400

    fixed isolatedModule issues

--- src/actions/__mocks__/index.ts
+++ src/actions/__mocks__/index.ts
@@ -1,14 +1,14 @@
-// module.exports = {
-//   ...jest.requireActual(".."),
-//   __esModule: true,
-//   // TODO: update return value for Redux / Context API
-//   getSecretWord: jest.fn().mockReturnValue(Promise.resolve("party")),
-// };
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
+module.exports = {
   ...jest.requireActual(".."),
   __esModule: true,
   // TODO: update return value for Redux / Context API
   getSecretWord: jest.fn().mockReturnValue(Promise.resolve("party")),
 };
+
+// eslint-disable-next-line import/no-anonymous-default-export
+// export default {
+//   ...jest.requireActual(".."),
+//   __esModule: true,
+//   // TODO: update return value for Redux / Context API
+//   getSecretWord: jest.fn().mockReturnValue(Promise.resolve("party")),
+// };
