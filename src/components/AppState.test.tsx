802ada99183d4be9abcbac79a91ab1ad2dc72d88
Joon Seo An <joonseo.an@outlook.com>
2021-12-15 07:59:40 -0500

    edit

--- src/components/AppState.test.tsx
+++ src/components/AppState.test.tsx
@@ -1,11 +1,17 @@
-/** Functional Test */
-// 1. in put props from the most parents
-// 2, simulate the change
-// 3. use mount(<App />) for the children component test
+// Embedded Context
 
-import { findByTestAttr } from "../test/testUtil";
+/** Add describe and beforeEach/After */
+import React from "react";
 import { mount, ReactWrapper } from "enzyme";
 import App from "./App";
+import { findByTestAttr } from "../test/testUtil";
+
+import GuessedWords from "./GuessWord_2/GuessWord";
+import Congrats from "./Congrats_1/Congrats";
+import Input from "./Input/Input";
+
+import successContext from "./context/successContext";
+import guessWordsContext from "./context/guessWordsContext";
 
 interface GuessedWord {
   guessedWord: string;
@@ -17,22 +23,34 @@ interface DefaultState {
   guessedWords?: GuessedWord[];
 }
 
-const setup = ({
-  success = false,
-  secretWord = "",
-  guessedWords = [],
-}: DefaultState) => {
-  // So now we test "guess a word every single time something changes"
+/**
+ * Factory function to create a ShallowWrapper for the GuessedWords component.
+ * @function setup
+ * @param {Array} guessedWords guessedWords value specific to this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = ({ secretWord, guessedWords }: DefaultState) => {
+  const wrapper = mount(
+    <guessWordsContext.GuessWordsProvider>
+      <successContext.SuccessProvider>
+        <Congrats />
+        <Input secretWord={secretWord} />
+        <GuessedWords />
+      </successContext.SuccessProvider>
+    </guessWordsContext.GuessWordsProvider>
+  );
 
-  // @TODO: apply state.
-  const wrapper = mount(<App />);
-
-  // guessed word
+  // add value to input box
   const inputBox = findByTestAttr(wrapper, "input-box");
-  inputBox.simulate("change", { target: { value: "train" } });
+  inputBox.simulate("change", { target: { value: "train " } });
 
+  // simulate click on submit button
   const submitButton = findByTestAttr(wrapper, "submit-button");
   submitButton.simulate("click", { preventDefault() {} });
+
+  guessedWords!.map(({ guessedWord }: GuessedWord) => {
+    const mockEvent = { target: { value: guessedWord } };
+  });
 
   return wrapper;
 };
@@ -109,3 +127,117 @@ describe.skip("guess secret word", () => {
     expect(submitButton.exists()).toBe(false);
   });
 });
+
+// ------------------------------------
+
+// /** Functional Test */
+// // 1. in put props from the most parents
+// // 2, simulate the change
+// // 3. use mount(<App />) for the children component test
+
+// import { findByTestAttr } from "../test/testUtil";
+// import { mount, ReactWrapper } from "enzyme";
+// import App from "./App";
+
+// interface GuessedWord {
+//   guessedWord: string;
+//   letterMatchCount: number;
+// }
+// interface DefaultState {
+//   success?: boolean;
+//   secretWord?: string;
+//   guessedWords?: GuessedWord[];
+// }
+
+// const setup = ({
+//   success = false,
+//   secretWord = "",
+//   guessedWords = [],
+// }: DefaultState) => {
+//   // So now we test "guess a word every single time something changes"
+
+//   // @TODO: apply state.
+//   const wrapper = mount(<App />);
+
+//   // guessed word
+//   const inputBox = findByTestAttr(wrapper, "input-box");
+//   inputBox.simulate("change", { target: { value: "train" } });
+
+//   const submitButton = findByTestAttr(wrapper, "submit-button");
+//   submitButton.simulate("click", { preventDefault() {} });
+
+//   return wrapper;
+// };
+
+// describe.skip("no words guessed", () => {
+//   let wrapper: ReactWrapper;
+
+//   beforeEach(() => {
+//     wrapper = setup({
+//       secretWord: "party",
+//       success: false,
+//       guessedWords: [],
+//     });
+//   });
+
+//   test("creates GuessedWords table with one row", () => {
+//     const guessedWordRow = findByTestAttr(wrapper, "guessed-word");
+//     expect(guessedWordRow).toHaveLength(1);
+//   });
+// });
+
+// describe.skip("some words guessed", () => {
+//   let wrapper: ReactWrapper;
+
+//   beforeEach(() => {
+//     wrapper = setup({
+//       secretWord: "party",
+//       success: false,
+//       // "a" matched in party and agile.
+//       guessedWords: [{ guessedWord: "agile", letterMatchCount: 1 }],
+//     });
+//   });
+
+//   test("creates GuessedWords table with one row", () => {
+//     const guessedWordRow = findByTestAttr(wrapper, "guessed-word");
+//     // why 2?
+//     expect(guessedWordRow).toHaveLength(2);
+//   });
+// });
+
+// describe.skip("guess secret word", () => {
+//   let wrapper: ReactWrapper;
+
+//   beforeEach(() => {
+//     wrapper = setup({
+//       secretWord: "party",
+//       success: false,
+//       // "a" matched in party and agile.
+//       guessedWords: [{ guessedWord: "agile", letterMatchCount: 1 }],
+//     });
+
+//     const inputBox = findByTestAttr(wrapper, "input-box");
+//     inputBox.simulate("change", { target: { value: "party" } });
+
+//     const submitButton = findByTestAttr(wrapper, "submit-button");
+//     submitButton.simulate("click", { preventDefault() {} });
+//   });
+
+//   test("add row to guessedWords table", () => {
+//     const guessedWordNodes = findByTestAttr(wrapper, "guessed-word");
+//     expect(guessedWordNodes).toHaveLength(3);
+//   });
+
+//   test("displays congrats component", () => {
+//     const congrats = findByTestAttr(wrapper, "component-congrats");
+//     expect(congrats.text().length).toBeGreaterThan(0);
+//   });
+
+//   test("does not display input component contents", () => {
+//     const inputBox = findByTestAttr(wrapper, "input-box");
+//     expect(inputBox.exists()).toBe(false);
+
+//     const submitButton = findByTestAttr(wrapper, "submit-button");
+//     expect(submitButton.exists()).toBe(false);
+//   });
+// });
--- src/components/GuessWord_2/GuessWord.test.tsx
+++ src/components/GuessWord_2/GuessWord.test.tsx
@@ -1,16 +1,11 @@
-// With Embedded Context
+// ------------------------- With Embedded Context --------------------------------
 
 /** Add describe and beforeEach/After */
 import React from "react";
-import { mount } from "enzyme";
-import App from "../App";
-import { findByTestAttr } from "../../test/testUtil";
-
+import { shallow, ShallowWrapper } from "enzyme";
+import { findByTestAttr, checkProps } from "../../test/testUtil";
 import GuessedWords from "./GuessWord";
-import Congrats from "../Congrats_1/Congrats";
-import Input from "../Input/Input";
 
-import successContext from "../context/successContext";
 import guessWordsContext from "../context/guessWordsContext";
 
 // [important]
@@ -26,36 +21,14 @@ import guessWordsContext from "../context/guessWordsContext";
  * @param {Array} guessedWords guessedWords value specific to this setup.
  * @returns {ShallowWrapper}
  */
-const setup = ({
-  secretWord,
-  guessedWords,
-}: {
-  secretWord: string;
-  guessedWords: [];
-}) => {
-  const wrapper = mount(
-    <guessWordsContext.GuessWordsProvider>
-      <successContext.SuccessProvider>
-        <Congrats />
-        <Input secretWord={secretWord} />
-        <GuessedWords />
-      </successContext.SuccessProvider>
-    </guessWordsContext.GuessWordsProvider>
-  );
+const setup = (guessedWords = []) => {
+  // on the guessWordContext, the array will return [value, setter]
+  const mockUseGuessedWords = jest
+    .fn()
+    .mockReturnValue([guessedWords, jest.fn]);
+  guessWordsContext.useGuessWords = mockUseGuessedWords;
 
-  // add value to input box
-  const inputBox = findByTestAttr(wrapper, "input-box");
-  inputBox.simulate("change", { target: { value: "train " } });
-
-  // simulate click on submit button
-  const submitButton = findByTestAttr(wrapper, "submit-button");
-  submitButton.simulate("click", { preventDefault() {} });
-
-  guessedWords.map(({ guessedWord }: { guessedWord: string }) => {
-    const mockEvent = { target: { value: guessedWord } };
-  });
-
-  return wrapper;
+  return shallow(<GuessedWords />);
 };
 
 describe("if there are not words guessed", () => {
@@ -229,208 +202,3 @@ describe("languagePicker", () => {
 //     expect(guessInstructions.text()).toBe(`🤔🤫🔤`);
 //   });
 // });
-
-// ------------------------- With Embedded Context --------------------------------
-
-// /** Add describe and beforeEach/After */
-// import React from "react";
-// import { shallow, ShallowWrapper } from "enzyme";
-// import { findByTestAttr, checkProps } from "../../test/testUtil";
-// import GuessedWords from "./GuessWord";
-
-// import guessWordsContext from "../context/guessWordsContext";
-
-// // [important]
-// // [ Embedded Context - Mock custom hook : mock return value sets context value]
-// // pros: 1) isolate unit test
-// // 2) does not rely on other functionality
-// // 3) can use shallow (isolate from child components)
-// // cons
-// // 1) need to export / import context object. otherwise mock won't work.
-// /**
-//  * Factory function to create a ShallowWrapper for the GuessedWords component.
-//  * @function setup
-//  * @param {Array} guessedWords guessedWords value specific to this setup.
-//  * @returns {ShallowWrapper}
-//  */
-// const setup = (guessedWords = []) => {
-//   // on the guessWordContext, the array will return [value, setter]
-//   const mockUseGuessedWords = jest
-//     .fn()
-//     .mockReturnValue([guessedWords, jest.fn]);
-//   guessWordsContext.useGuessWords = mockUseGuessedWords;
-
-//   return shallow(<GuessedWords />);
-// };
-
-// describe("if there are not words guessed", () => {
-//   let wrapper: ShallowWrapper;
-
-//   beforeEach(() => {
-//     wrapper = setup([]);
-//   });
-
-//   test("renders without error", () => {
-//     const component = findByTestAttr(wrapper, "component-guess-word");
-//     expect(component.length).toBe(1);
-//   });
-
-//   test("renders instructions to guess a word", () => {
-//     const instructions = findByTestAttr(wrapper, "guess-instructions");
-//     expect(instructions.text().length).not.toBe(0);
-//   });
-// });
-
-// describe("if there are words guessed", () => {
-//   let wrapper: ShallowWrapper;
-
-//   const guessedWords = [
-//     { guessedWord: "train", letterMatchCount: 3 },
-//     { guessedWord: "agile", letterMatchCount: 1 },
-//     { guessedWord: "party", letterMatchCount: 5 },
-//   ];
-
-//   beforeEach(() => {
-//     // for a while.
-//     wrapper = setup(guessedWords as []);
-//   });
-
-//   test("renders without error", () => {
-//     const component = findByTestAttr(wrapper, "component-guess-word");
-//     expect(component.length).toBe(1);
-//   });
-
-//   test('renders "guessed words" section', () => {
-//     const guessedWordNode = findByTestAttr(wrapper, "guessed-words");
-//     expect(guessedWordNode.length).toBe(1);
-//   });
-
-//   test("correct number of guessed words", () => {
-//     const guessedWordsNodes = findByTestAttr(wrapper, "guessed-word");
-//     expect(guessedWordsNodes.length).toBe(guessedWords.length);
-//   });
-// });
-
-// // [ Context ]
-// // Context Receiver Test without Provider by using mock
-// describe("languagePicker", () => {
-//   test("correctly renders guess instructions string in English by default", () => {
-//     const wrapper = setup([]);
-//     const guessInstructions = findByTestAttr(wrapper, "guess-instructions");
-//     expect(guessInstructions.text()).toBe("Try to guess the secret word!");
-//   });
-
-//   test("correctly renders guess instructions string in Emoji by default", () => {
-//     // Mocking return value from jest.fn()
-//     /**
-//      * [Important]
-//      * jest.fn().mockReturnValue(value) ===> to define any return value from a function.
-//      */
-//     const mockUseContext = jest.fn().mockReturnValue("emoji");
-//     // if return value is `emoji` from the useContext
-//     React.useContext = mockUseContext; // const returnValue = useContext(languageContext);
-//     const wrapper = setup([]);
-//     const guessInstructions = findByTestAttr(wrapper, "guess-instructions");
-//     expect(guessInstructions.text()).toBe(`🤔🤫🔤`);
-//   });
-// });
-
-// // ----------------------------------------------------------
-
-// // With Context api with props
-
-// // /** Add describe and beforeEach/After */
-// // import React from "react";
-// // import { shallow, ShallowWrapper } from "enzyme";
-// // import { findByTestAttr, checkProps } from "../../test/testUtil";
-// // import GuessedWords from "./GuessWord";
-
-// // const defaultProps = {
-// //   guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
-// // };
-
-// // /**
-// //  * Factory function to create a ShallowWrapper for the GuessedWords component.
-// //  * @function setup
-// //  * @param {Object} props Component props specific to this setup.
-// //  * @returns {ShallowWrapper}
-// //  */
-// // const setup = (props = {}) => {
-// //   const setupProps = { ...defaultProps, ...props };
-// //   return shallow(<GuessedWords {...setupProps} />);
-// // };
-
-// // test("does not throw warning with expected props", () => {
-// //   checkProps(GuessedWords, defaultProps);
-// // });
-
-// // describe("if there are not words guessed", () => {
-// //   let wrapper: ShallowWrapper;
-
-// //   beforeEach(() => {
-// //     wrapper = setup({ guessedWords: [] });
-// //   });
-
-// //   test("renders without error", () => {
-// //     const component = findByTestAttr(wrapper, "component-guess-word");
-// //     expect(component.length).toBe(1);
-// //   });
-
-// //   test("renders instructions to guess a word", () => {
-// //     const instructions = findByTestAttr(wrapper, "guess-instructions");
-// //     expect(instructions.text().length).not.toBe(0);
-// //   });
-// // });
-
-// // describe("if there are words guessed", () => {
-// //   let wrapper: ShallowWrapper;
-
-// //   const guessedWords = [
-// //     { guessedWord: "train", letterMatchCount: 3 },
-// //     { guessedWord: "agile", letterMatchCount: 1 },
-// //     { guessedWord: "party", letterMatchCount: 5 },
-// //   ];
-
-// //   beforeEach(() => {
-// //     wrapper = setup({ guessedWords });
-// //   });
-
-// //   test("renders without error", () => {
-// //     const component = findByTestAttr(wrapper, "component-guess-word");
-// //     expect(component.length).toBe(1);
-// //   });
-
-// //   test('renders "guessed words" section', () => {
-// //     const guessedWordNode = findByTestAttr(wrapper, "guessed-words");
-// //     expect(guessedWordNode.length).toBe(1);
-// //   });
-
-// //   test("correct number of guessed words", () => {
-// //     const guessedWordsNodes = findByTestAttr(wrapper, "guessed-word");
-// //     expect(guessedWordsNodes.length).toBe(guessedWords.length);
-// //   });
-// // });
-
-// // // [ Context ]
-// // // Context Receiver Test without Provider by using mock
-// // describe("languagePicker", () => {
-// //   test("correctly renders guess instructions string in English by default", () => {
-// //     const wrapper = setup({ guessedWords: [] });
-// //     const guessInstructions = findByTestAttr(wrapper, "guess-instructions");
-// //     expect(guessInstructions.text()).toBe("Try to guess the secret word!");
-// //   });
-
-// //   test("correctly renders guess instructions string in Emoji by default", () => {
-// //     // Mocking return value from jest.fn()
-// //     /**
-// //      * [Important]
-// //      * jest.fn().mockReturnValue(value) ===> to define any return value from a function.
-// //      */
-// //     const mockUseContext = jest.fn().mockReturnValue("emoji");
-// //     // if return value is `emoji` from the useContext
-// //     React.useContext = mockUseContext; // const returnValue = useContext(languageContext);
-// //     const wrapper = setup({ guessedWords: [] });
-// //     const guessInstructions = findByTestAttr(wrapper, "guess-instructions");
-// //     expect(guessInstructions.text()).toBe(`🤔🤫🔤`);
-// //   });
-// // });
